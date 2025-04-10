99e773621a741d6efb77d3be18c463b461a00cd7
Yash Dhasade <yash.dhasade@nimapinfotech.com>
2025-04-10 18:28:37 +0530

    April 10

---
+++ April 10.js
@@ -0,0 +1,41 @@
+
+// let number = [1,3,4,6,7,9,0,6,4,3,4];
+// let number2 = [1,3,4,6,7,9,0,6,4,3,4];
+// let isEqual =true
+//     if(number.length != number2.length){
+//         isEqual = false
+//     }
+//     for(let i = 0; i<number.length; i++){
+//         let check =false
+//         for(let j = 0; j<number2.length; j++){
+//                if(number[i] == number2[j]){
+//                    number2[j] = null
+//                    check =true;
+//                   break;
+//                }
+//         }
+//         if(!check){
+//             isEqual = false
+//             break;
+//         }
+//     }
+
+// console.log(isEqual)
+
+
+// ////////////////////
+// for(let i = 0; i<str.length; i++){
+//     let isRepeating = false
+//     for(let j = 0; j<str.length; j++){
+//         if(i == j){
+//             continue;
+//         }
+//         if(str[i] == str[j]){
+//             isRepeating = true
+//         }
+//     }
+//     if(!isRepeating){
+//         console.log(str[i])
+//         break
+//     }
+// }
---
+++ ReviewPrep/Review1.js
@@ -0,0 +1,109 @@
+// ************Closures
+
+// function increment(){
+//     let counter = 0;
+//     return function(){
+//         counter++;
+//         console.log(counter)
+//     }
+// };
+
+// let inc = increment();
+
+// inc();
+// inc();
+
+///// Lexical Scope
+// let number = 42;
+
+// function innerFunction(){
+//     console.log(number)
+// }
+// function outer(){
+//     let number = 3;
+//     innerFunction()
+// }
+
+// outer()
+
+// ***********Scope Chaining
+
+// export function Abc() {
+    
+//     this.yash = "yash";
+//     return ()=> {
+//         console.log(this.yash);
+//     }
+// }
+
+// let fn = new Abc();
+// fn()
+
+    // //  lexical scope
+    // function outer() {
+    //     let name = "John";
+        
+    //     function inner() {
+    //         console.log(name);  
+    //     }
+        
+    //     return inner;
+    // }
+    
+    // const closureFunc = outer();
+    // closureFunc(); 
+    
+    // const obj = {
+    //     name: "John",
+    //     printName: function () {
+    //         console.log(this.name);
+    //     }
+    // };
+    
+    // const print = obj.printName;
+    // print();  // ❌ Undefined (this refers to global object)
+    // obj.printName();  // ✅ "John" (this refers to obj)
+
+
+
+    ////////////////
+    // if (true) {
+    //     const y = 5;
+    //     var z = 10;
+    //   }
+    //   console.log(y); // What logs?
+    //   console.log(z); // What logs?
+    //   const y = 5;
+
+// // //////////////////
+
+//     function foo() {
+//         console.log(bar); // What logs?
+//       }
+//       function baz() {
+//         var bar = "surprise!";
+//         foo();
+//       }
+//       baz();
+
+
+    // // ******************Constructer function
+    // function Person(name, age) {
+    //     this.name = name;
+    //     this.age = age;
+    //     this.counter = 0
+    // }
+    
+    // // Adding methods to the prototype
+    // Person.prototype.sayHello = function () {
+    //     this.counter++
+    //     console.log(this.counter);
+    // };
+    // Person.prototype.newVar = 5
+    
+    // // Creating an instance
+    // const person1 = new Person("Yash", 25);
+    // person1.sayHello(); // Output: Hello, my name is Yash and I am 25 years old.
+    // person1.sayHello(); // Output: Hello, my name is Yash and I am 25 years old.
+    // console.log(person1.newVar);
+
